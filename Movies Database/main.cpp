0789f24f970ed82283f244a8effb46a4e854792e
PriyaPilla4 <65250086+PriyaPilla4@users.noreply.github.com>
2020-08-21 15:23:31 -0500

    Create main.cpp

---
+++ Movies Database/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  ProjectThree
+//
+//  Created by Priya Pilla on 3/20/20.
+//  Copyright © 2020 Priya Pilla. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include "menu.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    Menu menu;
+    
+    menu.StartMenu();
+    
+
+}
